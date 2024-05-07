3c61d3a9588e4b550d9e04b872a3f7a698b84746
DoumSof99 <sofiadmn99@gmail.com>
2024-05-07 16:50:48 +0300

    Init

---
+++ Tattoo_Calculator/Tattoo_Calculator/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Tattoo_Calculator {
+    public partial class App : Application {
+        public App() {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Tattoo_Calculator/Tattoo_Calculator/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,8 @@
+﻿using Foundation;
+
+namespace Tattoo_Calculator {
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
